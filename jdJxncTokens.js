3db924e1f777512d9f305b3277b98c7cd16c3fda
pukarziu <silence.fangyadi@Gmail.com>
2021-02-07 16:29:21 +0800

    #update jxnc

--- jdJxncTokens.js
+++ jdJxncTokens.js
@@ -8,10 +8,6 @@
 let JxncTokens = [
   '',//账号一的京喜农场token
   '',//账号二的京喜农场token
-  '',//账号三的京喜农场token
-  '',//账号四的京喜农场token
-  '',//账号五的京喜农场token
-
 ]
 // 判断github action里面是否有京喜农场 token 
 if (process.env.JXNCTOKENS) {
@@ -25,7 +21,7 @@ if (process.env.JXNCTOKENS) {
     JxncTokens = process.env.JXNCTOKENS.split();
   }
 } else if (process.env.JD_COOKIE) {
-  console.log(`由于您secret里面未提供 tokens，当种植 APP 种子时，将不能正常进行任务，请提供 token 或 种植非 APP 种子！`)
+  console.log(`由于您环境变量里面未提供 tokens，当种植 APP 种子时，将不能正常进行任务，请提供 token 或 种植非 APP 种子！`)
 }
 for (let i = 0; i < JxncTokens.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
